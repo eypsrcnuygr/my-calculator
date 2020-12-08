5ac1eecf85f6f9b1957ed9a19c2c334b0727b233
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-12-08 20:45:40 +0300

    Change the index.js into arrow func

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,13 +2,11 @@ import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-function App() {
-  return (
-    <>
-      <Display result="15" />
-      <ButtonPanel />
-    </>
-  );
-}
+const App = () => (
+  <>
+    <Display result="15" />
+    <ButtonPanel />
+  </>
+);
 
 export default App;
